8946e96db24cfe95e3d9ba616b0f668e0d30fe5f
Link4Electronics <link4electronics@gmail.com>
2026-01-23 21:10:46 -0300

    remove pipewire

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/512x512/apps/devilutionx.png
 export DESKTOP=/usr/share/applications/devilutionx.desktop
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/devilutionx
