4406c79303c7b86a43c519f5c19103795f7b70ab
Link4Electronics <link4electronics@gmail.com>
2026-02-06 20:40:17 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,9 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm cmake \
+pacman -Syu --noconfirm \
+    cmake       \
+    libdecor    \
     ninja       \
     fmt         \
     libsodium   \
